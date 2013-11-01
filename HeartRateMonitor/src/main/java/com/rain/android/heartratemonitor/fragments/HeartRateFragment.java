04816ecc90ee17c4d8ef23c89ed1000aefe9a75e
Jeremiah Stephenson <jeremiahs@mediarain.com>
2013-11-01 14:35:51 -0600

    Fixing my commit

--- HeartRateMonitor/src/main/java/com/rain/android/heartratemonitor/fragments/HeartRateFragment.java
+++ HeartRateMonitor/src/main/java/com/rain/android/heartratemonitor/fragments/HeartRateFragment.java
@@ -225,7 +225,7 @@ public class HeartRateFragment extends Fragment {
             final int bpm = mHeartRateProvider.getBpm();
             if (bpm > -1) {
                 getActivity().setProgressBarIndeterminateVisibility(false);
-                mBpm.setText(String.valueOf(bpm));
+                mBpm.setText(getString(R.string.bpm, bpm));
 
                 if (bpm > 0) {
 
